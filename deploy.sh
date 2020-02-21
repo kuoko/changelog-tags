c8bfec5d09f0a09ce17d5ee8bcb3c573638b213a
Antonio Romano <antonio.romano@metro.co.uk>
2020-02-21 12:25:41 +0000

    remove pretty

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 DEPLOY_BRANCH=$1
 COMMIT_HASH=$2
-TAG=$(date +'%Y-%d-%m/%H-%M-%S')
 
+TAG=$(date +'%Y-%d-%m/%H-%M-%S')
 if [[ 'master-built' = ${DEPLOY_BRANCH} ]]; then
     git checkout master
-    export LOG=$(git log -1 --pretty=%B)
+    export LOG=$(git log -1)
     git tag -a ${TAG} -m ${TAG} -m "${LOG}"
     git push origin tag ${TAG}
 fi
