7e01b80f5c785b7f82aa24dfd798ba7f0658b82b
Antonio Romano <antonio.romano@metro.co.uk>
2020-02-21 12:50:07 +0000

    get last tag and changelog

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 DEPLOY_BRANCH=$1
-COMMIT_HASH=$2
 
-TAG=$(date +'%Y-%d-%m/%H-%M-%S')
 if [[ 'master-built' = ${DEPLOY_BRANCH} ]]; then
+
     git checkout master
+    git pull
+
+    export LAST_TAG=$(git describe --tags `git rev-list --tags --max-count=1`)
+    export CHANGE_LOG=$(git log ${LAST_TAG}..origin/preprod --oneline)
+
     export LOG=$(git log -1)
-    git tag -a ${TAG} -m ${TAG} -m "${LOG}"
-    git push origin tag ${TAG}
+
+    NEW_TAG=$(date +'%Y-%d-%m/%H-%M-%S')
+    git tag -a ${NEW_TAG} -m ${NEW_TAG} -m "${LOG}" -m "${CHANGE_LOG}"
+    git push origin tag ${NEW_TAG}
 fi
